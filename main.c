d13b332983643081441f2c100f52850a38488367
benben159 <bennybroz105@gmail.com>
2019-01-24 01:04:54 +0000

    fix printf argument

--- main.c
+++ main.c
@@ -6,7 +6,7 @@ int main(int argc, char **argv) {
 	float humidity, temperature;
 	int rv = pi_dht_read(DHT11, 4, &humidity, &temperature);
 	if (rv == DHT_SUCCESS){
-		printf("humidity: %f; temperature: %f deg C\n", humidity, temperature);
+		printf("humidity: %.2f%%; temperature: %.2f deg C\n", humidity, temperature);
 	} else if (rv == DHT_ERROR_TIMEOUT) {
 		fprintf(stderr, "sensor reading timeout, try again later\n");
 	} else if (rv == DHT_ERROR_GPIO) {
